897aa82cc8699568f5dcc5d66686f3037d24af86
MattDahEpic <mattdahepic@gmail.com>
2015-06-21 22:42:17 -0600

    sttll doesnt work

--- src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
+++ src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
@@ -2,22 +2,22 @@ package com.mattdahepic.mdecore.misc;
 
 import com.mattdahepic.mdecore.MDECore;
 import com.mattdahepic.mdecore.helpers.LogHelper;
-import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
-import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class WaterproofRedstone {
     public static void doIt () {
         try {
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
             //wire
             Class wire_class = Blocks.redstone_wire.getClass().getSuperclass();
             Field wire_material = wire_class.getDeclaredField("blockMaterial");
             wire_material.setAccessible(true);
+            modifiersField.setInt(wire_material,wire_material.getModifiers() & ~Modifier.FINAL);
             wire_material.set(Blocks.redstone_wire, MDECore.waterproof_circuits);
-            System.out.println(Blocks.redstone_wire.getMaterial().toString());
             //repeat for other redstone type blocks, but since im probably doing it wrong i dont want to type all them up yet
         } catch (Exception e) {
             LogHelper.error(MDECore.MODID,"EVERYTHING DED, SOMETHING DOESNT EXIST");
