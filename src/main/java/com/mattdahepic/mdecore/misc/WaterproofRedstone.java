f6bc3e18e4426cfdc30470df9d17430cce1bdb4a
MattDahEpic <mattdahepic@gmail.com>
2015-06-21 21:36:25 -0600

    it boots! im shoe glad i got that working | water still breaks redstone doe

--- src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
+++ src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
@@ -13,9 +13,11 @@ public class WaterproofRedstone {
     public static void doIt () {
         try {
             //wire
-            Class c = Blocks.redstone_wire.getClass();
-            Field redstone_wire = c.getField("blockMaterial");
-            redstone_wire.set(Blocks.redstone_wire,MDECore.waterproof_circuits);
+            Class wire_class = Blocks.redstone_wire.getClass().getSuperclass();
+            Field wire_material = wire_class.getDeclaredField("blockMaterial");
+            wire_material.setAccessible(true);
+            wire_material.set(Blocks.redstone_wire, MDECore.waterproof_circuits);
+            System.out.println(Blocks.redstone_wire.getMaterial().toString());
             //repeat for other redstone type blocks, but since im probably doing it wrong i dont want to type all them up yet
         } catch (Exception e) {
             LogHelper.error(MDECore.MODID,"EVERYTHING DED, SOMETHING DOESNT EXIST");
