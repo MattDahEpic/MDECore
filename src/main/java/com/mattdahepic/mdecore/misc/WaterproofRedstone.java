c052921c8387c4a6865b9ec90a0055b4aea7e382
MattDahEpic <mattdahepic@gmail.com>
2015-06-21 21:07:22 -0600

    im bad at this

--- src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
+++ src/main/java/com/mattdahepic/mdecore/misc/WaterproofRedstone.java
@@ -2,6 +2,8 @@ package com.mattdahepic.mdecore.misc;
 
 import com.mattdahepic.mdecore.MDECore;
 import com.mattdahepic.mdecore.helpers.LogHelper;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 
@@ -10,11 +12,13 @@ import java.lang.reflect.Field;
 public class WaterproofRedstone {
     public static void doIt () {
         try {
-            Class c = Material.class;
-            Field circuits = c.getDeclaredField("circuits");
-            circuits.set(new Material(MapColor.airColor),MDECore.waterproof_circuits);
+            //wire
+            Class c = Blocks.redstone_wire.getClass();
+            Field redstone_wire = c.getField("blockMaterial");
+            redstone_wire.set(Blocks.redstone_wire,MDECore.waterproof_circuits);
+            //repeat for other redstone type blocks, but since im probably doing it wrong i dont want to type all them up yet
         } catch (Exception e) {
-            LogHelper.error(MDECore.MODID,"EVERYTHING DED, circuits DOESNT EXIST");
+            LogHelper.error(MDECore.MODID,"EVERYTHING DED, SOMETHING DOESNT EXIST");
             throw new RuntimeException(e);
         }
     }
