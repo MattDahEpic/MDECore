0f2c5072141e7cd6acf68312913e5a27e058658e
Matt <contact@mattdahepic.com>
2016-02-06 17:09:05 -0700

    finalize mdecore 1.8.9-1.0 for MattDahEpic/RingkyDinks#3

---
+++ src/main/java/com/mattdahepic/mdecore/command/AbstractCommand.java
@@ -0,0 +1,87 @@
+/*package com.mattdahepic.mdecore.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.CommandNotFoundException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.BlockPos;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+import java.util.*;
+
+public abstract class AbstractCommand extends CommandBase {
+    private static Map<String, ICommandLogic> commands = new HashMap<String,ICommandLogic>();
+
+    public static void init(FMLServerStartingEvent e) {
+        e.registerServerCommand(instance);
+    }
+    public static String getCommandSyntax (String name) {
+        if (getCommandExists(name)) {
+            return commands.get(name).getCommandSyntax();
+        }
+        return null;
+    }
+    public static boolean registerCommandLogic (ICommandLogic commandLogic) {
+        if (!commands.containsKey(commandLogic.getCommandName())) {
+            commands.put(commandLogic.getCommandName(), commandLogic);
+            return true;
+        }
+        return false;
+    }
+    public static Set<String> getCommandList() {
+        return commands.keySet();
+    }
+    public static int getCommandPermission (String command) {
+        return getCommandExists(command) ? commands.get(command).getPermissionLevel() : Integer.MAX_VALUE;
+    }
+    public static boolean getCommandExists (String command) {
+        return commands.containsKey(command);
+    }
+    public static boolean canUseCommand (ICommandSender sender, int requiredPermission, String name) {
+        if (getCommandExists(name)) {
+            return sender.canCommandSenderUseCommand(requiredPermission, getCommandName()+" "+name) || (sender instanceof EntityPlayerMP && requiredPermission <= 0);
+        }
+        return false;
+    }
+    @Override
+    public int getRequiredPermissionLevel () {
+        return -1;
+    }
+    @Override
+    public abstract String getCommandName ();
+    @Override
+    public List getCommandAliases () {
+        return Collections.emptyList();
+    }
+    @Override
+    public String getCommandUsage (ICommandSender sender) {
+        return "/"+getCommandName()+" help";
+    }
+    @Override
+    public boolean canCommandSenderUseCommand (ICommandSender sender) {
+        return true;
+    }
+    @Override
+    public void processCommand (ICommandSender sender, String[] args) throws CommandException {
+        if (args.length < 1) args = new String[]{"help"};
+        ICommandLogic command = commands.get(args[0]);
+        if (command != null) {
+            if (canUseCommand(sender,command.getPermissionLevel(),command.getCommandName())) {
+                command.handleCommand(sender,args);
+                return;
+            }
+            throw new CommandException("commands.generic.permission");
+        }
+        throw new CommandNotFoundException();
+    }
+    @Override
+    public List addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args,commands.keySet());
+        } else if (commands.containsKey(args[0])) {
+            return commands.get(args[0]).addTabCompletionOptions(sender,args,pos);
+        }
+        return null;
+    }
+}*/
