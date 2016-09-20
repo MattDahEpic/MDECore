222d985243abe7085605866dd7ff86fa3d42cbbe
MattDahEpic <contact@mattdahepic.com>
2016-09-19 18:13:43 -0600

    fix nullpointer in cauldron tweak & bump version, resolves #7

--- src/main/java/com/mattdahepic/mdecore/tweaks/WaterBottleCauldron.java
+++ src/main/java/com/mattdahepic/mdecore/tweaks/WaterBottleCauldron.java
@@ -14,18 +14,20 @@ public class WaterBottleCauldron {
     public void playerInteract (PlayerInteractEvent.RightClickBlock e) {
         if (MDEConfig.waterBottlesFillCauldrons) {
             if (e.getWorld().getBlockState(e.getPos()).getBlock() == Blocks.CAULDRON) { //if right click on cauldron
-                if (e.getEntityPlayer().inventory.getCurrentItem().getItem() == Items.POTIONITEM && e.getEntityPlayer().inventory.getCurrentItem().getMetadata() == 0) { //if water bottle
-                    int currentLevel = e.getWorld().getBlockState(e.getPos()).getValue(BlockCauldron.LEVEL);
-                    if (currentLevel < 3) { //if not full
-                        e.getWorld().setBlockState(e.getPos(), e.getWorld().getBlockState(e.getPos()).withProperty(BlockCauldron.LEVEL, currentLevel + 1)); //fill cauldron
-                        if (!e.getEntityPlayer().capabilities.isCreativeMode) {
-                            e.getEntityPlayer().inventory.getCurrentItem().stackSize--; //remove one water bottle
-                            if (!e.getEntityPlayer().inventory.addItemStackToInventory(new ItemStack(Items.GLASS_BOTTLE))) { //give the bottle back, but if they cant fit it...
-                                e.getWorld().spawnEntityInWorld(new EntityItem(e.getWorld(), e.getPos().getX() + 0.5D, e.getPos().getY() + 0.5D, e.getPos().getZ() + 0.5D, new ItemStack(Items.GLASS_BOTTLE))); //...drop it!
+                try {
+                    if (e.getEntityPlayer().inventory.getCurrentItem().getItem() == Items.POTIONITEM && e.getEntityPlayer().inventory.getCurrentItem().getMetadata() == 0) { //if water bottle
+                        int currentLevel = e.getWorld().getBlockState(e.getPos()).getValue(BlockCauldron.LEVEL);
+                        if (currentLevel < 3) { //if not full
+                            e.getWorld().setBlockState(e.getPos(), e.getWorld().getBlockState(e.getPos()).withProperty(BlockCauldron.LEVEL, currentLevel + 1)); //fill cauldron
+                            if (!e.getEntityPlayer().capabilities.isCreativeMode) {
+                                e.getEntityPlayer().inventory.getCurrentItem().stackSize--; //remove one water bottle
+                                if (!e.getEntityPlayer().inventory.addItemStackToInventory(new ItemStack(Items.GLASS_BOTTLE))) { //give the bottle back, but if they cant fit it...
+                                    e.getWorld().spawnEntityInWorld(new EntityItem(e.getWorld(), e.getPos().getX() + 0.5D, e.getPos().getY() + 0.5D, e.getPos().getZ() + 0.5D, new ItemStack(Items.GLASS_BOTTLE))); //...drop it!
+                                }
                             }
                         }
                     }
-                }
+                } catch (NullPointerException ignored) {}
             }
         }
     }
