1c784edab3ad1ce82772a10b82f4c906a1e09ae4
MattDahEpic <contact@mattdahepic.com>
2017-02-25 10:37:14 -0700

    switch telemetry provider

--- src/main/java/com/mattdahepic/mdecore/network/StatReporter.java
+++ src/main/java/com/mattdahepic/mdecore/network/StatReporter.java
@@ -1,31 +1,42 @@
 package com.mattdahepic.mdecore.network;
 
-import com.dmurph.tracking.AnalyticsConfigData;
-import com.dmurph.tracking.JGoogleAnalyticsTracker;
-import com.dmurph.tracking.system.AWTSystemPopulator;
-import com.mattdahepic.mdecore.MDECore;
 import com.mattdahepic.mdecore.helpers.EnvironmentHelper;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ModContainer;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatReporter {
     //By using the mod you agree to the EULA: http://mattdahepic.com/mods/eula
     public static void gatherAndReport () {
-        //setup google analytics tracker
-        JGoogleAnalyticsTracker.setProxy(System.getenv("http_proxy"));
-        AnalyticsConfigData config = new AnalyticsConfigData("UA-46943413-4");
-        AWTSystemPopulator.populateConfigData(config);
-        JGoogleAnalyticsTracker tracker = new JGoogleAnalyticsTracker(config, JGoogleAnalyticsTracker.GoogleAnalyticsVersion.V_4_7_2);
-        tracker.setDispatchMode(JGoogleAnalyticsTracker.DispatchMode.MULTI_THREAD);
-        //do reporting
-        for (ModContainer mod : Loader.instance().getActiveModList()) {
+        //setup
+        final HttpClient httpclient = HttpClients.createDefault();
+        //report
+        for (final ModContainer mod : Loader.instance().getActiveModList()) {
             if (mod.getMetadata().authorList.contains("MattDahEpic") || mod.getMetadata().authorList.contains("mattdahepic")) {
-                String modPage = mod.getModId()+"-"+mod.getVersion()+(EnvironmentHelper.isServer?"-server":"-client")+(EnvironmentHelper.isDeobf?"-deobf":"");
-                MDECore.logger.debug("Logging load for mod \""+modPage+"\".");
-                tracker.trackPageView(modPage, null, null); //modid-version-(server|client)-deobf
+                Thread t = new Thread(() -> {
+                    try {
+                        HttpPost httppost = new HttpPost("https://telemetry.mattdahepic.com/submit/1/");
+                        //prep request
+                        List<NameValuePair> params = new ArrayList<>(3);
+                        params.add(new BasicNameValuePair("product", mod.getModId()));
+                        params.add(new BasicNameValuePair("version", mod.getVersion() + (EnvironmentHelper.isServer ? "-server" : "-client")));
+                        params.add(new BasicNameValuePair("action", EnvironmentHelper.isDeobf ? "dev" : "run"));
+                        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+                        //do request
+                        httpclient.execute(httppost);
+                    } catch (Exception ignored) {}
+                });
+                t.setName("MattDahEpic Telemetry Thread");
+                t.start();
             }
         }
-        //cleanup
-        tracker.completeBackgroundTasks(1000);
     }
 }
